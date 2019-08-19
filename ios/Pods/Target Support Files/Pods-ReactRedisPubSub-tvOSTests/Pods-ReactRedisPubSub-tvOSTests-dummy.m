29c86e5ef6ca349ee69f0ff855b36be5a78931e0
Guilherme Assis <guilherme.assis@codificar.com.br>
2019-08-19 18:32:30 -0300

    remocao pasta Pods

--- ios/Pods/Target Support Files/Pods-ReactRedisPubSub-tvOSTests/Pods-ReactRedisPubSub-tvOSTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_ReactRedisPubSub_tvOSTests : NSObject
-@end
-@implementation PodsDummy_Pods_ReactRedisPubSub_tvOSTests
-@end
--- ios/Pods/Target Support Files/Pods-ReactRedisPubSub/Pods-ReactRedisPubSub-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_ReactRedisPubSub : NSObject
-@end
-@implementation PodsDummy_Pods_ReactRedisPubSub
-@end
